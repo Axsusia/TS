04cf64c00f089f82e4b86bb2b2052aefa437285e
Axsusia <zion437@gmail.com>
2014-03-01 15:03:40 +0900

    대업데이트
    고고고고 첸

--- TS.1.0.js
+++ TS.1.0.js
@@ -1,15 +1,20 @@
 /*************************************************************
 name : TS Plug-in
-ver	 : 1.0
-date : 2013.12.29
-blog : zion437.tistory.com
+ver	 : 1.01
+date : 2014.02.12
+blog : http://zion437.tistory.com
 Copyright(c)2013~ Axsusia(엑수시아) All rights reserved. 
 *************************************************************/
 if (!jQuery) { throw new Error("TS javascript requires jQuery") }
 var TS = TS || (function(){
-	var _version = "1.0";
-	var _name = "Axsusia(엑수시아)";
+	"use strict";
+	//var _version = "1.01";
+	//var _name = "Axsusia(엑수시아)";
 	var _blog = 'http://zion437.tistory.com';
+	var _setBlog = function(x){
+		_getBlog = x;
+	}
+	var _getBlog = '';
 	var _setUrl = function(x){
 		_getUrl = x;
 	}
@@ -18,7 +23,7 @@ var TS = TS || (function(){
 	module.locator = (function(){
 		var _i			= _i	|| ' > ';
 		var _home		= _home || 'HOME';
-		var _iLi		= function(){return '<li class="ltIcon"><span>'+_i+'</span></li>';}
+		var _iLi		= function(){return '<li class="ltIcon"><span>' + _i + '</span></li>';}
 		var cateKey		= function(){return _categoryName.nameKey();}
 		var title		= function(){return $('.articleTitle').eq(0).text();}
 		var titleSize	= function(){return $('.articleTitle').size();}
@@ -26,9 +31,15 @@ var TS = TS || (function(){
 			var _html = '';
 			_html += '<ul>';
 			_html += '<li class="ltLink"><a href="/">'+_home+'</a></li>';
-			if('search,guestbook,media,location,tag'.indexOf(_url.urlKey()) != -1){
+			if('search,guestbook,media,location,tag,notice'.indexOf(_url.urlKey()) != -1){
 				_html += _iLi();
 				_html += '<li class="ltLink"><a href="/'+_url.urlKey()+'">'+_url.urlKey()+'</a></li>';
+			}else if('archive'.indexOf(_url.urlKey()) != -1){
+				_keyName = cateKey()[0] + cateKey()[1];
+				_html += _iLi();
+				_html += '<li class="ltLink"><a href="/'+_url.urlKey()+'">'+_url.urlKey()+'</a></li>';
+				_html += _iLi();
+				_html += '<li class="ltLink"><a href="/'+_url.urlKey()+'/'+_keyName+'">'+_keyName+'</a></li>';
 			}else if(fn.isNotNull(cateKey()[0])){
 				_html += _iLi();
 				_html += '<li class="ltLink"><a href="/category/'+cateKey()[0]+'">'+cateKey()[0]+'</a></li>';
@@ -36,10 +47,10 @@ var TS = TS || (function(){
 					_html += _iLi();
 					_html += '<li class="ltLink"><a href="/category/'+cateKey()[0]+'/'+cateKey()[1]+'">'+cateKey()[1]+'</a></li>';
 				}
-			}
-			if(fn.isNotNull(title()) && _url.urlKey() != 'list' && titleSize() == 1){
-				_html += _iLi();
-				_html += '<li class="ltLink"><a href="'+decodeURIComponent(location.href).split('#')[0]+'">'+title()+'</a></li>';
+				if(fn.isNotNull(title()) && _url.urlKey() != 'list' && titleSize() == 1){
+					_html += _iLi();
+					_html += '<li class="ltLink"><a href="'+decodeURIComponent(location.href).split('#')[0]+'">'+title()+'</a></li>';
+				}
 			}
 			_html += '</ul>';
 			return _html;
@@ -69,7 +80,7 @@ var TS = TS || (function(){
 	})();
 	module.subCategory = (function(){
 		var _logic = function(){
-			var a = _categoryName.nameKey()[0];
+			var a = _key().name;
 			var z = '';
 			if(fn.isNotNull(a)){
 				z = _category.categoryAry()[a];
@@ -78,6 +89,21 @@ var TS = TS || (function(){
 		}
 		return {
 			getHTML : _logic
+		}
+	})();
+	module.object =(function(){
+		var _set = function(){
+			
+		}
+		var _logic = function(){
+			$('.imageblock').css('width','auto');
+			$('.imageblock').css('height','auto');
+			$('.imageblock').css('display','block');
+			//medio 부분 필요함
+			
+		}
+		return{
+			setAutoSize : _logic
 		}
 	})();
 	var fn = fn || {};
@@ -96,37 +122,87 @@ var TS = TS || (function(){
 		return z;
 	};
 	var ajax = ajax || {};
-	ajax.data = ajax.data || new Array();
-	ajax.call = function(x){
-		if(!fn.isNotNull(ajax.data[x])){}
+	ajax.data = ajax.data || [];
+	ajax.call = function(x, dataType, callback){
+		// if ajax data is null 
+		if(!fn.isNotNull(ajax.data[x])){	// ajax call for data to setting 
+			$.ajax({
+				type : 'post',
+				async : 'false',
+				url : _getBlog + '/' + x,
+				dataType : dataType,
+				contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+				success : function (response, status, request){
+					// for test
+					console.log('after ajax data ----------------');
+					console.log(response);
+					/*
+					최소한의 데이터 호출을 위한 방편
+					*/
+					ajax.data[x] = response;
+					if(callback != null) callback(ajax.data[x]);
+				},
+				error : function (request, status, error){
+					ajax.error();
+				},
+				complete : function (){
+					//comlete msg or doing
+
+				}
+			})
+		}else{
+			//if hava the data just callback!!
+			if(callback != null) callback(ajax.data[x]);
+		}
+	};
+	ajax.mgt = function(){
+		// 관리 및 로직 실행용
+		var listData ;
+		var pagingData ;
+		
+
+	};
+	ajax.error = function(){
+
 	};
 	var _key = function(){
 		var urlKey = _url.urlKey();
 		var cateKey = _categoryName.nameKey();
 		var findKey;
 
-		if(_findKey(cateKey[0]).name != '')					{findKey = cateKey[0];}
-		if(_findKey(cateKey[1]).name != '')					{findKey = cateKey[1];}
-		if(_findKey(urlKey).name != '')						{findKey = urlKey;}
+		if(_keySet[cateKey[0]] == 'Y')					{findKey = cateKey[0];}
+		if(_keySet[cateKey[1]] == 'Y')					{findKey = cateKey[1];}
+		if(_keySet[urlKey] == 'Y')						{findKey = urlKey;}
 		if(fn.isNotNull(urlKey) && urlKey == 'list'){
-			if(_findKey(urlKey+'_'+cateKey[0]).name != '')	{findKey = urlKey+'_'+cateKey[0];}
-			if(_findKey(urlKey+'_'+cateKey[1]).name != '')	{findKey = urlKey+'_'+cateKey[1];}
+			if(_keySet[urlKey+'_'+cateKey[0]] == 'Y')	{findKey = urlKey+'_'+cateKey[0];}
+			if(_keySet[urlKey+'_'+cateKey[1]] == 'Y')	{findKey = urlKey+'_'+cateKey[1];}
+			if(_keySet[urlKey+'_'+cateKey[0]+cateKey[1]] == 'Y')	{findKey = urlKey+'_'+cateKey[0]+cateKey[1];}
 		}
-		if(!fn.isNotNull(findKey))							{findKey = '#!#$*$#!#';}
+		if(findKey == '' || findKey == null)				{findKey = cateKey[0];}
+		//console.log(findKey+'<<<findKey what?');
 		return _findKey(findKey);
 	};
 	var _keyArray = _keyArray || new Array();
+	var _keySet = _keySet || new Array();
 	var _setting = function(x){
-		this.keyData = this.keyData || new Array();
 		var a = x.name	|| '';
 		if(fn.isNotNull(a)){
-			this.keyData[a] = x;
-			_keyArray = this.keyData;
+			_keySet[x.name] = 'Y';
+			_keyArray[a] = x;
 		};
 	};
+	var _settingCss = function(x){
+		$('head').append('<link rel="stylesheet" type="text/css" href="'+_getUrl+x+'" />');
+	}
+	var _settingJs = function(x){
+		$.getScript(_getUrl+x, function(){});
+	}
+	var _settingHtml = function(){
+		//ajax  call for html
+	}
 	var _findKey = function(x){
 		if(!fn.isNotNull(_keyArray[x])){
-			return {name:'',css:'',js:''}
+			return {name:x,css:'',js:''}
 		}else{
 			return _keyArray[x];
 		}
@@ -153,6 +229,8 @@ var TS = TS || (function(){
 				if(y[1] == 'media')		{z = 'media';}
 				if(y[1] == 'location')	{z = 'location';}
 				if(y[1] == 'tag')		{z = 'tag';}
+				if(y[1] == 'notice')	{z = 'notice';}
+				if(y[1] == 'archive')	{z = 'archive';}
 			}
 			return z;
 		}
@@ -186,8 +264,7 @@ var TS = TS || (function(){
 		categoryAry : function(){
 			var a = new Array();
 			$('#category ul ul').each(function(idx){
-				var _cate = $(this).clone();
-				var _cateClone = _cate.html();
+				_cateClone = $(this).clone().html();
 				$(_cateClone).each(function(index){
 					_htmlVal = $(this).find('ul').html();
 					_keyVal = $(this).find('a').filter(':first').text();
@@ -202,22 +279,27 @@ var TS = TS || (function(){
 		var key = _key();
 		var css	= key.css	== '' ? '' : key.css;
 		var js	= key.js	== '' ? '' : key.js;
-		if(fn.isNotNull(css))	{$('head').append('<link rel="stylesheet" type="text/css" href="'+_getUrl+css+'" />');}
-		if(fn.isNotNull(js))	{$.getScript(_getUrl+js, function(){});}
+		var html= key.html  == '' ? '' : key.html;
+		if(fn.isNotNull(css))	{_settingCss(css)}
+		if(fn.isNotNull(js))	{_settingJs (js)}
+		if(fn.isNotNull(html))	{_settingHtml(html)}
 	};
 
 	return {
-		ver		: _version,
-		name	: _name,
-		blog	: _blog,
-		setUrl	: _setUrl,
-		setting : _setting,
-		key		: _key,
-		start	: _start,
-		url		: _url,
-		module	: module,
-		fn		: fn,
-		ajax	: ajax
+		//ver			: _version,
+		//name		: _name,
+		//blog		: _blog,
+		setUrl		: _setUrl,
+		setBlog 	: _setBlog,
+		setting		: _setting,
+		settingCss	: _settingCss,
+		settingJs	: _settingJs,
+		key			: _key,
+		start		: _start,
+		url			: _url,
+		module		: module,
+		fn			: fn,
+		ajax		: ajax
 	}
 })();
 
